4c3811d5b39211afa4d37426d51540bf115e3755
Omid Houshyar <ohoushyar@gmail.com>
2012-05-08 02:38:31 +0800

    added isRemovable property

--- js/tickable.js
+++ js/tickable.js
@@ -6,12 +6,14 @@ Ava.Tickable = function (spec) {
     var that = {};
 
     var note = spec.note;
+    var isRemovable = spec.isRemovable || false;
     var x,
         y,
         next,
         prev;
 
     that.note = note;
+    that.isRemovable = isRemovable;
     that.x = x;
     that.y = y;
     that.next = next;
