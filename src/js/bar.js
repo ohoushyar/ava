b167409480d382ab606c18bd45cfc1bfabf494fb
Omid Houshyar <ohoushyar@gmail.com>
2013-05-02 23:06:26 +0800

    renamed measure to bar

--- src/js/bar.js
+++ src/js/bar.js
@@ -1,57 +1,37 @@
 /*
- * Measure
+ * Bar
  *
  * Bar (or measure) is a segment of time defined by a given number of
  * beats, each of which are assigned a particular note value
  * (https://en.wikipedia.org/wiki/Bar_%28music%29)
  *
  */
-Ava.Measure = function (spec) {
+Ava.Bar = function (spec) {
     var that = {};
 
     // Public
-    // that.x;
-    // that.width;
+    //
     // Public Methods
     // that.addTickable;
-    // that.draw;
 
     // Private attribute
-    var y,
-        ctx,
-        first,
+    var first,
         current,
         last,
         clef,
-        showClef,
-        keySignature,
-        numBeat,
-        beatValue,
-        showTimeSignature,
-        timeSignature,
-        nextMeasure,
-        prevMeasure,
+        show_clef,
+        key_signature,
+        num_beat,
+        beat_value,
+        show_time_signature,
+        time_signature,
+        next_bar,
+        prev_bar,
         time,
         voice,
-        stave,
-        formatter,
-        errorContainer,
-        formatter,
-        default_width = 250,
-        pixel_per_note = 50;
-
-    /*
-     * calc_width
-     *
-     * private
-     *
-     */
-    var calc_width = function() {
-        return default_width;
-
-        var width = (pixel_per_note * that.num_tickables);
-        return  width < default_width ? default_width : width;
-    };
+        stave;
+        // default_width = 250,
+        // pixel_per_note = 50;
 
     /*
      * push_to_link
@@ -72,7 +52,7 @@ Ava.Measure = function (spec) {
             };
         }
 
-        // Set first to the new note if it's the first one in measure
+        // Set first to the new note if it's the first one in Bar
         if (typeof first !== 'object') {
             first = tickable;
             current = first;
@@ -113,7 +93,7 @@ Ava.Measure = function (spec) {
 
         var rests = [];
 
-        // Fill up the rest of measure with rest and flag it as removable
+        // Fill up the rest of Bar with rest and flag it as removable
         if (!voice.isComplete()) {
             // Get the number of ticks to fill with removable rest
             var rest_ticks = voice.getTotalTicks() - voice.getTicksUsed();
@@ -150,13 +130,11 @@ Ava.Measure = function (spec) {
                     push_to_link(rests[i]);
                 }
                 catch (e) {
-                    $(errorContainer).html('[Measure] ' + e.code + ': ' + e.message);
+                    $(errorContainer).html('[Bar] ' + e.code + ': ' + e.message);
                     throw e;
                 }
             }
 
-            // Set the possible changed width
-            setWidth(calc_width());
         }
     };
 
@@ -167,11 +145,7 @@ Ava.Measure = function (spec) {
     (function(spec) {
         var new_note;
 
-        that.x = spec.x || 10;
-        y = spec.y || 0;
         that.num_tickables = 0;
-
-        ctx        = spec.ctx;
 
         // Init list of notes
         for (var i=0; i < spec.tickables.length; i+=1) {
@@ -190,65 +164,36 @@ Ava.Measure = function (spec) {
             push_to_link(new_note);
         }
 
-        that.width = spec.width || calc_width();
-
         clef = spec.clef;
-        showClef = spec.showClef || false;
+        show_clef = spec.show_clef || false;
 
-        keySignature = spec.keySignature;
+        key_signature = spec.key_signature;
 
-        numBeat = spec.numBeat || 4;
-        beatValue = spec.beatValue || 4;
-        showTimeSignature = spec.showTimeSignature || false;
+        num_beat = spec.num_beat || 4;
+        beat_value = spec.beat_value || 4;
+        show_time_signature = spec.show_time_signature || false;
 
-        timeSignature = numBeat + "/" + beatValue;
-
-        stave = new Vex.Flow.Stave(that.x, y, that.width);
-        stave.setContext(ctx);
-
-        errorContainer = "#error-msg";
+        time_signature = num_beat + "/" + beat_value;
 
         time = {
-            num_beats: numBeat,
-            beat_value: beatValue,
+            num_beats: num_beat,
+            beat_value: beat_value,
             resolution: Vex.Flow.RESOLUTION,
         };
 
-        try {
-            current = first;
-
-            voice = new Vex.Flow.Voice(time).setStrict(true);
-            while (current) {
-                voice.addTickable(current.note);
-                current = current.next;
-            }
-        }
-        catch (e) {
-            $(errorContainer).html('[Measure] ' + e.code + ': ' + e.message);
-            throw e;
-        }
-
+        current = first;
 
         fill_with_removable_rest();
 
+        // if (key_signature !== undefined) {
+        //     if (key_signature != "C")
+        //         setWidth(that.width+15);
+        // }
 
-        if (clef !== undefined && showClef) {
-            stave.addClef(clef);
-            setWidth(that.width+15);
-        }
-
-        if (keySignature !== undefined) {
-            stave.addKeySignature(keySignature);
-            if (keySignature != "C")
-                setWidth(that.width+15);
-        }
-
-        if (showTimeSignature) {
-            stave.addTimeSignature(timeSignature);
-            setWidth(that.width+15);
-        }
-
-        formatter = new Vex.Flow.Formatter();
+        // if (show_time_signature) {
+        //     stave.addTimeSignature(time_signature);
+        //     setWidth(that.width+15);
+        // }
 
     }(spec));
 
@@ -268,7 +213,7 @@ Ava.Measure = function (spec) {
         }
 
         // Reset the voice
-        voice = new Vex.Flow.Voice(time).setStrict(true);
+        // voice = new Vex.Flow.Voice(time).setStrict(true);
 
         current = first;
         while (current) {
@@ -284,77 +229,77 @@ Ava.Measure = function (spec) {
                 break;
             }
 
-            voice.addTickable(current.note);
+            // voice.addTickable(current.note);
             current = current.next;
         }
 
-        if (!voice.isComplete()) {
-            push_to_link(tickable);
-            voice.addTickable(tickable.note);
-            fill_with_removable_rest();
-        } else {
-            console.warn('Too many ticks');
-        }
+        // if (!voice.isComplete()) {
+        //     push_to_link(tickable);
+        //     voice.addTickable(tickable.note);
+        //     fill_with_removable_rest();
+        // } else {
+        //     console.warn('Too many ticks');
+        // }
     };
 
-    /*
-     * draw
-     *
-     * public
-     *
-     */
-    that.draw = function () {
-        // Do some hack to get the right justification width to pass to stave before
-        // running formatToStave
-        adjust_width();
+    // /*
+    //  * draw
+    //  *
+    //  * public
+    //  *
+    //  */
+    // that.draw = function () {
+    //     // Do some hack to get the right justification width to pass to stave before
+    //     // running formatToStave
+    //     adjust_width();
 
-        try {
-            stave.draw();
-            formatter.joinVoices([voice]).formatToStave([voice], stave);
-            voice.draw(ctx, stave);
-        }
-        catch (e) {
-            $(errorContainer).html('[Measure::draw] ' + e.code + ': ' + e.message);
-        }
-    };
+    //     try {
+    //         stave.draw();
+    //         formatter.joinVoices([voice]).formatToStave([voice], stave);
+    //         voice.draw(ctx, stave);
+    //     }
+    //     catch (e) {
+    //         $(errorContainer).html('[Measure::draw] ' + e.code + ': ' + e.message);
+    //     }
+    // };
 
-    var adjust_width = function() {
-        var current_width = get_current_width();
+    // var adjust_width = function() {
+    //     var current_width = get_current_width();
 
-        if (current_width >= that.width - stave.getNoteStartX())
-            setWidth(current_width + stave.getNoteStartX() + 200);
+    //     if (current_width >= that.width - stave.getNoteStartX())
+    //         setWidth(current_width + stave.getNoteStartX() + 200);
 
-    };
+    // };
 
-    // Well so far couldn't found a proper way of getting this value, so here is
-    // a work around, pretty messy to get it.
-    var get_current_width = function() {
-        // A magic to enable ticks to give their width ...
-        formatter.createTickContexts([voice]);
+    // // Well so far couldn't found a proper way of getting this value, so here is
+    // // a work around, pretty messy to get it.
+    // var get_current_width = function() {
+    //     // A magic to enable ticks to give their width ...
+    //     formatter.createTickContexts([voice]);
 
-        var contexts = formatter.tContexts;
-        var contextList = contexts.list;
-        var contextMap = contexts.map;
+    //     var contexts = formatter.tContexts;
+    //     var contextList = contexts.list;
+    //     var contextMap = contexts.map;
 
-        var minTotalWidth = 0;
+    //     var minTotalWidth = 0;
 
-        // Go through each tick context and calculate total width and smallest
-        // ticks.
-        for (var i = 0; i < contextList.length; ++i) {
-            var context = contextMap[contextList[i]];
+    //     // Go through each tick context and calculate total width and smallest
+    //     // ticks.
+    //     for (var i = 0; i < contextList.length; ++i) {
+    //         var context = contextMap[contextList[i]];
 
-            // preFormat() gets them to descend down to their tickables and modifier
-            // contexts, and calculate their widths.
-            context.preFormat();
-            minTotalWidth += context.getWidth();
+    //         // preFormat() gets them to descend down to their tickables and modifier
+    //         // contexts, and calculate their widths.
+    //         context.preFormat();
+    //         minTotalWidth += context.getWidth();
 
-            var minTicks = context.getMinTicks();
-            if (i == 0) this.minTicks = minTicks;
-            if (minTicks < this.minTicks) this.minTicks = minTicks;
-        }
+    //         var minTicks = context.getMinTicks();
+    //         if (i == 0) this.minTicks = minTicks;
+    //         if (minTicks < this.minTicks) this.minTicks = minTicks;
+    //     }
 
-        return minTotalWidth;
-    };
+    //     return minTotalWidth;
+    // };
 
     /*
      * has_empty_spot
@@ -374,10 +319,6 @@ Ava.Measure = function (spec) {
 
             return false;
     };
-
-    // Tmp Public
-    that._formatter = formatter;
-    that._stave = stave;
 
     return that;
 };
