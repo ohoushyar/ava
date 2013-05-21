ea65c8042e8e0e276868b27f5c4db678234f5566
Omid Houshyar <ohoushyar@gmail.com>
2013-05-22 02:24:00 +0800

    added more test to beam view

--- t/beam_view_test.js
+++ t/beam_view_test.js
@@ -102,7 +102,6 @@ var beam_view_test = function () {
     test(sub_module_name, function() {
 
         var test_title = 'Successfully ran render';
-
         ava_test_helper.run_view_test( module_name, sub_module_name, test_title, function(env) {
 
             var mod = _.clone(model);
@@ -121,6 +120,126 @@ var beam_view_test = function () {
             ok( (function(){
                     view.render();
                     return view.$el;
+                })(), test_title);
+        });
+
+        test_title = 'Successfully ran render with different position of beam';
+        ava_test_helper.run_view_test( module_name, sub_module_name, test_title, function(env) {
+
+            var mod = _.clone(model);
+            mod.ctx = env.ctx;
+            var mod_result = init(mod);
+
+            var beam_views = [
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes.slice(0, 4),
+                        ctx: mod_result.ctx,
+                    })
+                }),
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes.slice(8, 12),
+                        ctx: mod_result.ctx,
+                    })
+                })
+            ];
+            // Voice draw has to happen after instantiation of BeamView
+            mod_result.voice.draw(mod_result.ctx, mod_result.vex_stave);
+
+            // Then after voice draw run BeamView render otherwise it
+            // won't render properly. This is how VexFlow works.
+            ok( (function(){
+                    _.each(beam_views, function(view) {
+                        view.render();
+                    });
+                    return true;
+                })(), test_title);
+        });
+
+        test_title = 'Successfully ran render with different position of beams';
+        ava_test_helper.run_view_test( module_name, sub_module_name, test_title, function(env) {
+
+            var mod = _.clone(model);
+            mod.ctx = env.ctx;
+            var mod_result = init(mod);
+
+            var beam_views = [
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes.slice(0, 2),
+                        ctx: mod_result.ctx,
+                    })
+                }),
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes.slice(2, 4),
+                        ctx: mod_result.ctx,
+                    })
+                }),
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes.slice(8, 12),
+                        ctx: mod_result.ctx,
+                    })
+                })
+            ];
+            // Voice draw has to happen after instantiation of BeamView
+            mod_result.voice.draw(mod_result.ctx, mod_result.vex_stave);
+
+            // Then after voice draw run BeamView render otherwise it
+            // won't render properly. This is how VexFlow works.
+            ok( (function(){
+                    _.each(beam_views, function(view) {
+                        view.render();
+                    });
+                    return true;
+                })(), test_title);
+        });
+
+        test_title = 'Successfully ran render with automatic beams';
+        ava_test_helper.run_view_test( module_name, sub_module_name, test_title, function(env) {
+
+            var mod = {
+                notes: new Ava.TickableList([
+
+                    { keys: ["f/4"], duration: "16"},
+                    { keys: ["e/4"], duration: "8"},
+                    { keys: ["d/4"], duration: "16"},
+                    { keys: ["c/4"], duration: "16"},
+
+                    { keys: ["d/4"], duration: "16"},
+                    { keys: ["c/4"], duration: "16"},
+                    { keys: ["d/4"], duration: "8"},
+                    { keys: ["f/4"], duration: "16"},
+
+                    { keys: ["d/4"], duration: "8"},
+                    { keys: ["d/4"], duration: "q"},
+
+                ]),
+            };
+
+            mod.ctx = env.ctx;
+            var mod_result = init(mod);
+
+            var beam_views = [
+                new Ava.BeamView({
+                    model: new Backbone.Model({
+                        notes: mod_result.notes,
+                        ctx: mod_result.ctx,
+                    })
+                }),
+            ];
+            // Voice draw has to happen after instantiation of BeamView
+            mod_result.voice.draw(mod_result.ctx, mod_result.vex_stave);
+
+            // Then after voice draw run BeamView render otherwise it
+            // won't render properly. This is how VexFlow works.
+            ok( (function(){
+                    _.each(beam_views, function(view) {
+                        view.render();
+                    });
+                    return true;
                 })(), test_title);
         });
     });
