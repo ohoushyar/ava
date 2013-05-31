37c02d92c4793080117adf79abbcc81ccd17d8ae
Omid Houshyar <ohoushyar@gmail.com>
2013-06-01 01:33:14 +0800

    added constants

--- src/js/ava.js
+++ src/js/ava.js
@@ -3,9 +3,13 @@
  */
 Ava = ( function () {
     var that = {};
+
     that.Constant = {
         CANVAS:  'canvas',
         RAPHAEL: 'raphael',
+        BAR_CONTEXT_EXTRA_WIDTH: 5,
+        DEFAULT_WIDTH: 1024,
+        DEFAULT_HEIGHT: 120,
     };
 
     that.valid_duration = ['w', 'h', 'q', 8, 16, 32, 64];
@@ -13,27 +17,37 @@ Ava = ( function () {
     that.valid_key_signatures = ["C", "Am", "F", "Dm", "Bb", "Gm", "Eb", "Cm", "Ab", "Fm", "Db", "Bbm", "Gb", "Ebm", "Cb", "Abm", "G", "Em", "D", "Bm", "A", "F#m", "E", "C#m", "B", "G    #m", "F#", "D#m", "C#", "A#m"];
 
     that.Context = (function () {
-            var currDuration = 'w';
+        var currDuration = 'w';
 
-            return {
-                currentDuration: function (duration) {
+        return {
+            currentDuration: function (duration) {
 
-                    if (typeof duration !== 'undefined') {
-                        if ($.inArray(!isNaN(duration) ? parseInt(duration) : duration, that.valid_duration) == -1) {
-                            throw {
-                                name: 'typeError',
-                                message: "Invalid duration. Duration needs to be char"
-                            }
+                if (typeof duration !== 'undefined') {
+                    if ($.inArray(!isNaN(duration) ? parseInt(duration) : duration, that.valid_duration) == -1) {
+                        throw {
+                            name: 'typeError',
+                            message: "Invalid duration. Duration needs to be char"
                         }
-
-                        currDuration = duration;
                     }
 
-                    return currDuration;
-                },
-            };
-        }() );
+                    currDuration = duration;
+                }
 
+                return currDuration;
+            },
+        };
+    }() );
+
+    that.set_vexflow_context = function(ctx) {
+        if (typeof ctx !== 'object') {
+            throw {
+                name: 'invalidParam',
+                message: 'Invalid param. ctx has to be object',
+            }
+        }
+
+        that.Context.vexflow_ctx = ctx;
+    };
 
     return that;
 }() );
