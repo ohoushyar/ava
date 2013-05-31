f88d981274818e11b80fe6992dd9e46c26fd524e
Omid Houshyar <ohoushyar@gmail.com>
2013-06-01 01:32:05 +0800

    improved test helper

--- t/ava_test_helper.js
+++ t/ava_test_helper.js
@@ -6,31 +6,42 @@ var ava_test_helper = (function() {
 
     var last_container_num = Math.round(new Date().getTime()/1000.0);
     var unittest_div_prefex = 'unittest-';
+    var unittest_div_id;
+    var $unittest_div_element;
+    var $title;
 
-    var elements = function(div_id) {
+    var get_$div = function(div_id) {
         if ( div_id === undefined )
             alert('ERROR: no div_id');
 
         return $( '<div id="' + div_id + '"></div>' );
     };
 
-    that.init_env = function() {
+    that.get_unittest_div_and_title = function() {
         last_container_num += 1;
-        var unittest_div_id = unittest_div_prefex + last_container_num;
+        unittest_div_id = unittest_div_prefex + last_container_num;
 
-        var $unittest_div_element = $( '<div id="' + unittest_div_id + '"></div>' );
-        $unittest_div_element.append( elements(unittest_div_id + '-title') );
-        $unittest_div_element.append( elements(unittest_div_id + '-canvas') );
+        $unittest_div_element = $( '<div id="' + unittest_div_id + '"></div>' );
+        $title = get_$div(unittest_div_id + '-title');
+        $title.addClass('ut-title');
+        $unittest_div_element.append( $title );
 
         $("#test-env").append($unittest_div_element);
 
-        var $unittest_div = $('#'+unittest_div_id);
+        return {
+            div_id: unittest_div_id,
+            $div: $unittest_div_element,
+            $title: $title,
+        };
+    };
+
+    that.init_env = function() {
+        that.get_unittest_div_and_title();
+
+        $unittest_div_element.append( get_$div(unittest_div_id + '-canvas') );
 
         var $canvas = $("#"+unittest_div_id+"-canvas");
         $canvas.addClass("ut-canvas");
-
-        var $title = $("#"+unittest_div_id+"-title");
-        $title.addClass('ut-title');
 
         var renderer = new Vex.Flow.Renderer($canvas[0], Vex.Flow.Renderer.Backends.RAPHAEL);
 
