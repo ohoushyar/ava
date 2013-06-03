9002f3947cca1c06e488b918dcbee946586df8d2
Omid Houshyar <ohoushyar@gmail.com>
2013-06-04 01:49:20 +0800

    used closure in stave model

--- src/js/stave.js
+++ src/js/stave.js
@@ -1,45 +1,49 @@
-/*
- * stave
- * TODO:
- * - note should initialize here with parameter has been passed rather
- *   than passing vexflow stavenote
- */
-
-Ava.StaveModel = Backbone.Model.extend({
-    defaults: function() {
-        return {
-            x: 0,
-            y: 0,
-            width: 250,
-            clef: undefined,
-            time_signature: undefined,
-            key_signature: undefined,
-        };
-    },
-
-    initialize: function() {
-
-        if ( typeof this.get('clef') === 'string'
-            && !_.include(Ava.valid_clefs, this.get('clef')) ) {
-            throw {
-                name: 'initError',
-                message: 'Invalid clef',
-            };
-        }
-
-        if ( typeof this.get('key_signature') === 'string'
-            && !_.include(Ava.valid_key_signatures, this.get('key_signature')) ) {
-            throw {
-                name: 'initError',
-                message: 'Invalid key_signature',
-            };
-        }
-
-    },
-});
-
+/**
+ * Stave model
+ * @class Ava.Stave
+ * @constructor
+ * @param {Object} spec
+ **/
 Ava.Stave = function (spec) {
-    var that = new Ava.StaveModel(spec);
+    var that = {};
+
+    (function(spec) {
+        var Model = Backbone.Model.extend({
+            defaults: function() {
+                return {
+                    x: 0,
+                    y: 0,
+                    width: 250,
+                    clef: undefined,
+                    time_signature: undefined,
+                    key_signature: undefined,
+                };
+            },
+
+            initialize: function() {
+
+                if ( typeof this.get('clef') === 'string'
+                    && !_.include(Ava.valid_clefs, this.get('clef')) ) {
+                    throw {
+                        name: 'initError',
+                        message: 'Invalid clef',
+                    };
+                }
+
+                if ( typeof this.get('key_signature') === 'string'
+                    && !_.include(Ava.valid_key_signatures, this.get('key_signature')) ) {
+                    throw {
+                        name: 'initError',
+                        message: 'Invalid key_signature',
+                    };
+                }
+
+            },
+
+        });
+
+        that = new Model(spec);
+    })(spec);
 
     return that;
 };
