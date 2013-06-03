6869f0b38472761401c6766ec0ee80e68d38426d
Omid Houshyar <ohoushyar@gmail.com>
2013-06-03 18:36:15 +0800

    documentation

--- src/js/ava.js
+++ src/js/ava.js
@@ -1,6 +1,8 @@
-/*
- * Ava
- */
+/**
+ * @module Ava
+ * @class Ava
+ * @constructor
+ **/
 Ava = ( function () {
     var that = {};
 
@@ -18,6 +20,10 @@ Ava = ( function () {
     that.valid_clefs = ["treble", "bass", "alto", "tenor", "percussion"];
     that.valid_key_signatures = ["C", "Am", "F", "Dm", "Bb", "Gm", "Eb", "Cm", "Ab", "Fm", "Db", "Bbm", "Gb", "Ebm", "Cb", "Abm", "G", "Em", "D", "Bm", "A", "F#m", "E", "C#m", "B", "G    #m", "F#", "D#m", "C#", "A#m"];
 
+    /**
+     * @class Ava.Context
+     * @constructor
+     **/
     that.Context = (function () {
         var curr_duration = 'w';
         var vexflow_context_div_id;
@@ -41,6 +47,13 @@ Ava = ( function () {
             },
 
 
+            /**
+             * A global accessor to get/set ctx div id.
+             * @method vexflow_ctx_div_id
+             * @static
+             * @param {String} div_id
+             * @return {String} div_id
+             **/
             vexflow_ctx_div_id: function(div_id) {
                 if ( typeof div_id !== 'undefined' ) {
                     if ( !(typeof div_id === 'string' && $("#"+div_id).length) ) {
@@ -56,6 +69,13 @@ Ava = ( function () {
                 return vexflow_context_div_id;
             },
 
+            /**
+             * A global accessor to get/set ctx.
+             * @method vexflow_ctx
+             * @static
+             * @param {Object} ctx Vex.Flow.Renderer
+             * @return {Object} Vex.Flow.Renderer
+             **/
             vexflow_ctx: function(ctx) {
 
                 if (typeof ctx !== 'undefined') {
