8f7459c56e19aaf67d12a6a90ea43bad4377e3ae
Omid Houshyar <ohoushyar@gmail.com>
2013-10-18 04:01:10 +0800

    Added a method to return bar of corresponding x

--- src/js/music_view.js
+++ src/js/music_view.js
@@ -57,12 +57,47 @@ Ava.MusicView = function(spec) {
                 var bar_view = Ava.BarView( {model: bar_model} );
 
                 bar_view.render();
+                that.bars.push(bar_view);
+
             },
 
         });
 
         that = new View(spec);
+        that.bars = [];
     })(spec);
+
+    /**
+     * Return BarView object of passed x and y
+     * TODO: It could accept y when we have wrap enable
+     *
+     * @method get_bar_view_of
+     * @param {Number} x
+     * @return {Object} Ava.BarView
+     **/
+    that.get_bar_view_of = function( x ) {
+        var bar_x, bar_w, i;
+
+        if ( typeof x === 'undefined' ) {
+            throw {
+                name: 'reqParam',
+                message: "x is required"
+            };
+        }
+
+        for (i=0; i < that.bars.length; i+=1) {
+             bar_x = that.bars[i].model.get('x');
+             bar_w = that.bars[i].model.get('width');
+            if (x >= bar_x && x < bar_x + bar_w) {
+                return that.bars[i];
+            }
+        }
+
+        throw {
+            name: 'outOfRange',
+            message: "No bar found for x: " + x,
+        };
+    };
 
     return that;
 };
--- t/music_view_test.js
+++ t/music_view_test.js
@@ -84,6 +84,41 @@ var music_view_test = function () {
                 })(), test_title);
         });
 
+        test_title = 'Successfully rendered with four bars';
+        ava_test_helper.run_view_test( module_name, sub_module_name, test_title, function(env) {
+
+            var new_model = _.clone(model);
+            new_model.bars.push( {
+                notes: [
+                    { keys: ["d/4"], duration: "q" },
+                    { keys: ["c/4"], duration: "q" },
+                    { keys: ["b/4"], duration: "qr" },
+                    { keys: ["d/4"], duration: "q" },
+                ],
+            } );
+
+            var mod = Ava.Music(new_model);
+            mod.set('canvas_id', env.canvas_id);
+            var view = Ava.MusicView({ model: mod });
+            view.render();
+
+            ok( (function() {
+                    view.add_bar({
+                        notes: [
+                            { keys: ["d/5"], duration: "wr" },
+                        ],
+                    });
+                    return true;
+                })(), test_title);
+
+            throws( function() { view.get_bar_view_of(); }       , 'Exception has been thrown on invalid x');
+            throws( function() { view.get_bar_view_of( 2000 ); } , 'Exception has been thrown on out of range x');
+
+            equal( view.get_bar_view_of(100).model.get('x'), view.bars[0].model.get('x'), 'Got expected bar view object' );
+            equal( view.get_bar_view_of(600).model.get('x'), view.bars[1].model.get('x'), 'Got expected bar view object' );
+
+        });
+
     });
 
 };
