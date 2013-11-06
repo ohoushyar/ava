95668594503449eacbff20dae082d2d6dba9135b
Omid Houshyar <ohoushyar@gmail.com>
2013-11-07 03:12:42 +0800

    Added example to dispatcher

--- src/js/ava_dispatcher.js
+++ src/js/ava_dispatcher.js
@@ -1,5 +1,14 @@
 /**
  * Ava Dispatcher Class
+ * @example
+ *      // Set the event listener
+ *      this.listenTo( Ava.Dispatcher, 'toolbar:add_bar', function() { alert("Yeyyyy I'm listening"); } );
+ *
+ *      // Somewhere else:
+ *      // Trigger the event
+ *      that.$el.find('#toolbar-btn-add-bar')
+ *          .click( function() { Ava.Dispatcher.trigger( 'toolbar:add_bar' ); } );
+ *
  * @class Ava.Dispatcher
  **/
 Ava.Dispatcher = (function() {
